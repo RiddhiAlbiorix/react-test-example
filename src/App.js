aee7d7a1149bd1167d9bfc3134a4dfdc547df175
riddhi <riddhi.albiorix@gmail.com>
2021-10-05 17:36:44 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
+// import React from "react";
+
+// function App() {
+//   const [theme, setTheme] = useState("dark");
+
+//   const toggleTheme = () => {
+//     const nextTheme = theme === "dark" ? "light" : "dark";
+//     setTheme(nextTheme);
+//   };
+
+//   return <button onClick={toggleTheme}>
+//       Current theme: {theme}
+//     </button>;
+// }
+
+// export default App;
+
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -4,5 +4,26 @@ import App from './App';
 test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(linkElement).toBeInTheDocument;
 });
+
+// ------------------------------------------------------
+// import { render, screen } from "@testing-library/react";
+// import userEvent from "@testing-library/user-event";
+// import App from "./App";
+
+// test("Test theme button toggle", () => {
+//   render(<App />);
+//   const buttonEl = screen.getByText(/Current theme/i);
+    
+//   userEvent.click(buttonEl);
+//   expect(buttonEl).toHaveTextContent(/light/i);
+// });
+
+// ---------------------------------------------------------
+
+// const add = (a, b) => a + b;
+// test('should add two numbers', () => {
+//  const sum = add(3, 4);
+//  expect(sum).toBe(7);
+// });
